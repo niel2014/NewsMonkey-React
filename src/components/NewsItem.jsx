9f8bccb93d1b2687b7b6ca6c5ad62e0bfc35ac07
niel2014 <awadhesh.singh866@gmail.com>
2022-08-24 16:34:34 +0530

    Added app component structure

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is News item component</h1>
+      </div>
+    )
+  }
+}
