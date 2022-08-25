1efb1f98d97a11e150d58042befd6d7357fed641
niel2014 <awadhesh.singh866@gmail.com>
2022-08-25 18:23:23 +0530

    ceated news, newsitem, navbar components and fetched data form news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,10 +2,39 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      isLoading: false
+    }
+  }
+
+  async componentDidMount(){
+    const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=26eb3cdd7b2944fbb10579eec5c70be9";
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    this.setState({articles: parsedData.articles});
+  }
+
   render() {
     return (
-      <div>
-        <NewsItem />
+      <div className="container my-3">
+        <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            console.log(element)
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title? element.title.slice(0,60):""} description={element.description? element.description.slice(0,110):""}
+                imageUrl={element.urlToImage} url={element.url} author={element.author} source={element.source.name} />
+            </div>
+          })}
+
+        </div>
+        <div className="flex my-3" style={{justifyContent: "space-between"}}>
+        <button className='btn btn-primary btn-sm'>Prev </button>
+        <button className='btn btn-primary btn-sm'>Next </button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, url } = this.props;
     return (
-      <div>
-        <h1>This is News item component</h1>
+      <div className='my-3'>
+        <div className="card" style={{minHeight:"447px"}}>
+          <img src={imageUrl} alt="image" />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More </a>
+          </div>
+        </div>
       </div>
     )
   }
